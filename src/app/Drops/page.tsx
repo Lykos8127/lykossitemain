b5b77710e05da4514b41cf1d2c65ccd210acfb81
Lykos8127 <info@lykosmanufacture.com>
2025-07-11 18:36:05 +0200

    bug fix

---
+++ src/app/Drops/page.tsx
@@ -0,0 +1,13 @@
+import Main from "../components/video-bg"
+
+
+export default function Home() {
+  return(
+    <div className="bg-black h-[105] w-full">
+      <Main/>
+      
+   
+   
+    </div>
+  )
+}
